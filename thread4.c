076a9a3d827824e0b8c2c30e604b7ac79e4d81d5
Ankith Kumar <ankithkumar165@gmail.com>
2025-11-03 00:09:21 +0530

    Refactor thread handling and comments in thread4.c

--- thread4.c
+++ thread4.c
@@ -1,26 +1,24 @@
 // If main() finishes before the created thread (t1) completes, the entire process is terminated, including all threads.
-// Solution: Wait for the child thread to complete or only terminate main thread allowing other threads to continue.
+// Solution: Wait for the child thread to complete or only terminate the main thread allowing other threads to continue.
 
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 
 void *myfunc(void *arg){
-  
-  for(int i = 0 ; i < 5 ; i++){
-    printf("My Thread \n");
-    sleep(1);
-  }
-  return NULL;
+	for(int i = 0 ; i < 5 ; i++){
+    	printf("My Thread \n");
+        sleep(1);
+    }
+    return NULL;
 }
 
 int main(){
-  pthread_t t1;
-  pthread_create(&t1 , NULL, myfunc , NULL);
-  pthread_exit(NULL);
+    pthread_t t1;
+    pthread_create(&t1 , NULL, myfunc , NULL);
   
-  return 0;
+    pthread_exit(NULL);
 }
 
-// pthread_exit() prevents the main thread from terminating allowing other threads (like t1) to continue executing independently. 
+// pthread_exit() prevents the main thread from terminating and allow other threads (like t1) to continue the excution independently. 
 // It exits only the main thread, not the entire process.
